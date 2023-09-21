c507255f90dfa8af4c5607fb6a92d7c1e5bdaa13
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-21 13:05:58 -0700

    added l

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	long long int longlongintegerType;
 	float floatType;
 
-	printf("size of a char: %d byte(s)\n", (unsigned long) sizeof(char));
-	printf("size of an int: %d byte(s)\n", (unsigned long) sizeof(int));
-	printf("size of a long int: %d byte(s)\n", (unsigned long) sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", (unsigned long) sizeof(long long int));
-	printf("size of a float: %d byte(s)\n", (unsigned long) sizeof(float));
+	printf("size of a char: %ld byte(s)\n", (unsigned long) sizeof(char));
+	printf("size of an int: %ld byte(s)\n", (unsigned long) sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", (unsigned long) sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
