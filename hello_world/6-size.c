4520f9a394037115ecd9f20611e498bcd0a52508
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-21 13:30:57 -0700

    trying something new

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	long long int longlongintegerType;
 	float floatType;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(char));
-	printf("size of an int: %ld byte(s)\n", sizeof(int));
-	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
