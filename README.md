0d14fee4686461c84f39049f80fb7abe9e2d3504
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-22 10:58:49 -0700

    added directory and file

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository will be used to learn the C language
---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: creates random number
+ * If number is positive prints n is positive
+ * If n is equal to zero print n is zero
+ * If negative print n is negative
+ *
+ * Return - Always (0) (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
