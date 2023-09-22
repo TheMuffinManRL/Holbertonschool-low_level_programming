6643f5abf488ff2eeab82220ad8d269ed66f06db
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-22 11:17:06 -0700

    removed else statement

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,14 +17,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >= 1)
+	if (n > 1)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n <= 1)
+	if (n < 1)
 	{
 		printf("%d is negative\n", n);
-	} else
+	}
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
