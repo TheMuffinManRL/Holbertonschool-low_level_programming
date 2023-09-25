b5bb8ee290ab1087dead430afc32d3e7b176f744
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-25 12:18:25 -0700

    dont know

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,16 @@
 #include "main.h"
+/**
+* print_alphabet_x10 - print alphabet x10 lowercase
+*/
+void print_alphabet_x10(void)
+{
+	int contar = 0;
+	char letra;
 
+		while (contar++ <= 9)
+		{
+			for (letra = 'a'; letra <= 'z'; letra++)
+				_putchar(letra);
+			_putchar('\n');
+		}
+}
