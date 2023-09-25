00043d85c9b61ebd9abc56af11f552f7c9fad2a8
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-24 20:20:21 -0700

    added 5-print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: prints out numbers zero through nine in integers
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return : (0)
+}
