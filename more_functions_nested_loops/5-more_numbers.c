4b3f8cd7bd1ac33f790a5ef0995d3e773a6f1c51
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-09-27 11:53:48 -0700

    added 5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	char c = 0;
+
+	int i;
+
+	while (i < 11)
+	{
+		c = 0;
+		while (c < 15)
+		{
+			_putchar('0' + c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 2-mul - multiplies 2 integers together
 3-print_numbers - prints numbers 0-9 followed by a new line
 4-print_most_numbers - prints numbers 0-9 except 2 and 4
+5-more_numbers - prints numbers 0-14 ten time
