175863887ad8ae8ac6018d127c347ca86a7fc0a0
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-10-02 15:32:42 -0700

    added 0-reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the parameters value to 98
+ * @n: value originally given
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98 - changes the value of int n through a pointer to 98
