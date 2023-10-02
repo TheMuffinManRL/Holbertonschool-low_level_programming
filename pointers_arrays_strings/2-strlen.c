d9339ae3dd66379354b90d19b770505dd14f009e
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-10-02 15:55:00 -0700

    added 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: string given
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-reset_to_98 - changes the value of int n through a pointer to 98
 1-swap - swaps the value of two integers
+2-strlen - returns the amount of characters are in the string
 
