13b90ca14a11378d68de2c6d08d6e04539bff51c
TheMuffinManRL <ryanmcdaniel1123@gmail.com>
2023-10-02 16:49:09 -0700

    fixed betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/*
- * print_rev - prints a string, in reverse, followed by a new line
- * @s: string given
+/**
+ * print_rev - Prints a string in reverse with new line
+ *
+ * @s: String given
  */
 void print_rev(char *s)
 {
